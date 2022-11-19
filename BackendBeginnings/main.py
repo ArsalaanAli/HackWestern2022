247e6d64db81700edae5dcbc21626527a5941c34
Yang51st <86985694+Yang51st@users.noreply.github.com>
2022-11-19 09:15:36 -0500

    Put starter backend code into functions.

--- BackendBeginnings/main.py
+++ BackendBeginnings/main.py
@@ -20,39 +20,60 @@ examples = [
     Example("Expands to new horizons", "positive"),
 ]
 
-URL = "https://www.google.com/search?q=jesus&sxsrf=ALiCzsb3_47bWJyYZ73939rKgCDhXNcrNQ:1668852611519&source=lnms&tbm=nws&sa=X&ved=2ahUKEwiz6t24gLr7AhUbIjQIHVy1DEIQ_AUoA3oECAMQBQ&biw=1440&bih=821&dpr=2"
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
-headings = soup.find_all("h3")
-inputs = [i.find("div").text for i in headings]
 
-co = cohere.Client('yo3sGBCHSmIUSH3bQJNjzjYobJVka265uNZQ5Q8p')
-response = co.classify(
-    model="large",
-    inputs=inputs,
-    examples=examples,
-)
+def ParseForHeadlines(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    headings = soup.find_all("h3")
+    inputs = [i.find("div").text for i in headings]
+    return inputs
 
-sentiment = 0
-for i in range(len(inputs)):
-    if response.classifications[i].prediction == "negative":
-        sentiment -= response.classifications[i].confidence
-    if response.classifications[i].prediction == "positive":
-        sentiment += response.classifications[i].confidence
-sentiment /= len(inputs)
-sentiment += 1
-sentiment /= 2
 
-prompter = "This stock "+CATEGORIES[int(len(CATEGORIES)*sentiment)]
-response = co.generate(
-    model='medium',
-    prompt=prompter,
-    max_tokens=40,
-    temperature=0.6,
-    stop_sequences=["."])
+def ClassifyHeadlines(inputs):
+    co = cohere.Client('yo3sGBCHSmIUSH3bQJNjzjYobJVka265uNZQ5Q8p')
+    response = co.classify(
+        model="large",
+        inputs=inputs,
+        examples=examples,
+    )
+    return response
 
-stock_description = response.generations[0].text
-print(prompter+stock_description)
+
+def GetSentiment(inputs, response):
+    sentiment = 0
+    for i in range(len(inputs)):
+        print()
+        print(inputs[i])
+        print(response.classifications[i])
+        print()
+        if response.classifications[i].prediction == "negative":
+            sentiment -= response.classifications[i].confidence
+        if response.classifications[i].prediction == "positive":
+            sentiment += response.classifications[i].confidence
+    sentiment /= len(inputs)
+    sentiment += 1
+    sentiment /= 2
+    return sentiment
+
+
+def GenerateDescription(sentiment):
+    co = cohere.Client('yo3sGBCHSmIUSH3bQJNjzjYobJVka265uNZQ5Q8p')
+    prompter = "This stock "+CATEGORIES[int(len(CATEGORIES)*sentiment)]
+    response = co.generate(
+        model='medium',
+        prompt=prompter,
+        max_tokens=40,
+        temperature=0.6,
+        stop_sequences=["."])
+    stock_description = response.generations[0].text
+    print(prompter+stock_description)
+
+
+inputs = ParseForHeadlines(
+    "https://www.google.com/search?q=meta&sxsrf=ALiCzsaSPDrf7gvK_75WAWemS43p92q4JA:1668867145331&source=lnms&tbm=nws&sa=X&ved=2ahUKEwiCrv_Ktrr7AhW4IjQIHdFdC74Q_AUoAXoECAEQAw&biw=1440&bih=821&dpr=2")
+response = ClassifyHeadlines(inputs)
+sentiment = GetSentiment(inputs, response)
+GenerateDescription(sentiment)
 
 # Read the Room
 # Tough Crowd
