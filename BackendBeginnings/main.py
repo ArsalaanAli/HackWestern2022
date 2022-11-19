eb22ba8c9a176f98e97cd8f296efc56a632123ac
Yang51st <86985694+Yang51st@users.noreply.github.com>
2022-11-19 15:48:10 -0500

    Add in URLs.

--- BackendBeginnings/main.py
+++ BackendBeginnings/main.py
@@ -18,15 +18,27 @@ examples = [
     Example("Not as bad as you think.", "positive"),
     Example("Desperate measures being taken", "negative"),
     Example("Expands to new horizons", "positive"),
+    Example("Sentenced to 11 years in prison.", "negative"),
+    Example("Found innocent and aqcuitted", "positive"),
+    Example("An update is overdue.", "negative"),
+    Example("Fully supported with excellent customer service", "positive"),
+    Example("Obsolete and outdated.", "negative"),
+    Example("State-of-the-art.", "positive"),
 ]
 
 
 def ParseForHeadlines(url):
-    page = requests.get(url)
+    url = url.lower()
+    page = requests.get("https://www.google.com/search?q="+url +
+                        "&sxsrf=ALiCzsaSPDrf7gvK_75WAWemS43p92q4JA:1668867145331&source=lnms&tbm=nws&sa=X&ved=2ahUKEwiCrv_Ktrr7AhW4IjQIHdFdC74Q_AUoAXoECAEQAw&biw=1440&bih=821&dpr=2")
     soup = BeautifulSoup(page.content, "html.parser")
     headings = soup.find_all("h3")
+    hyperlinks = [a['href'] for a in soup.find_all('a', href=True) if a.text]
     inputs = [i.find("div").text for i in headings]
-    return inputs
+    hyperlinks = hyperlinks[-6-len(inputs):-6]
+    HeadlinesAndUrls = [(inputs[i], hyperlinks[i]) for i in range(len(inputs))]
+    # print(HeadlinesAndUrls[5][1][7:])
+    return HeadlinesAndUrls
 
 
 def ClassifyHeadlines(inputs):
@@ -42,38 +54,67 @@ def ClassifyHeadlines(inputs):
 def GetSentiment(inputs, response):
     sentiment = 0
     for i in range(len(inputs)):
+
         print()
         print(inputs[i])
         print(response.classifications[i])
         print()
+
         if response.classifications[i].prediction == "negative":
             sentiment -= response.classifications[i].confidence
         if response.classifications[i].prediction == "positive":
             sentiment += response.classifications[i].confidence
     sentiment /= len(inputs)
-    sentiment += 1
-    sentiment /= 2
     return sentiment
 
 
-def GenerateDescription(sentiment):
+def GenerateDescription(inputs, responses):
     co = cohere.Client('yo3sGBCHSmIUSH3bQJNjzjYobJVka265uNZQ5Q8p')
-    prompter = "This stock "+CATEGORIES[int(len(CATEGORIES)*sentiment)]
-    response = co.generate(
-        model='medium',
-        prompt=prompter,
-        max_tokens=40,
-        temperature=0.6,
-        stop_sequences=["."])
-    stock_description = response.generations[0].text
-    print(prompter+stock_description)
+    # "This stock "+CATEGORIES[int(len(CATEGORIES)*sentiment)]
+    TEMPLATE = """Generates a summary of the stock evaluation.
+Passage: A new venue has been opened.
+TLDR: A new venue has been opened.
+-
+Passage: Black Friday deals you can get.
+TLDR: Profits are expected to rise from Black Friday sales.
+-
+Passage: A new release has been announced.
+TLDR: Much-anticipated product offerings are slated for release.
+-
+"""
+    collector = []
+    for i in range(len(inputs)):
+        if responses.classifications[i].confidence > 0.7:
+            prompter = TEMPLATE
+            prompter += "Passage: "+inputs[i]+"\n"
+            prompter += "TLDR: "
+            response = co.generate(
+                model='medium',
+                prompt=prompter,
+                max_tokens=40,
+                temperature=0.3,
+                frequency_penalty=0.5,
+                stop_sequences=[".", ","])
+            stock_description = response.generations[0].text
+            collector.append(stock_description)
+        else:
+            collector.append("NULL")
+    return collector
 
 
-inputs = ParseForHeadlines(
-    "https://www.google.com/search?q=meta&sxsrf=ALiCzsaSPDrf7gvK_75WAWemS43p92q4JA:1668867145331&source=lnms&tbm=nws&sa=X&ved=2ahUKEwiCrv_Ktrr7AhW4IjQIHdFdC74Q_AUoAXoECAEQAw&biw=1440&bih=821&dpr=2")
-response = ClassifyHeadlines(inputs)
-sentiment = GetSentiment(inputs, response)
-GenerateDescription(sentiment)
+name = "target bad"
+HeadlinesAndUrls = ParseForHeadlines(name)
+titles = [i[0] for i in HeadlinesAndUrls]
+results = ClassifyHeadlines(titles)
+sentiment = GetSentiment(titles, results)
+print(CATEGORIES[int((sentiment+1)/2*len(CATEGORIES))])
+descriptions = GenerateDescription(titles, results)
+response = ClassifyHeadlines(descriptions)
+for i in range(len(descriptions)):
+    if response.classifications[i].confidence > 0.8 and descriptions[i] != "NULL":
+        print(descriptions[i])
+        print(HeadlinesAndUrls[i][1])
+        print()
 
 # Read the Room
 # Tough Crowd
