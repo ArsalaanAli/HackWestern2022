48f24ea6069e2d066ab0d635ff29242dc9bd505e
Arsalaan Ali <arsalaanali2@gmail.com>
2022-11-19 22:51:26 -0500

    Funtional Frontend

--- HackWestern2022/src/App.jsx
+++ HackWestern2022/src/App.jsx
@@ -1,12 +1,115 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import StockData from "../sample.json";
+import StockData from "../AllStocks.json";
+import TopStocks from "../TopStocks.json";
 
 function App() {
+  const MAX_STOCKS = 5;
+  const [curStocks, setCurStocks] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [stockInfo, setStockInfo] = useState({});
+
+  useEffect(() => {
+    const fetchData = async (stockName) => {
+      console.log("fetching");
+      const data = await fetch(
+        "https://94aac77b-2c55-492f-bca2-8fb32d5ff3c3.mock.pstmn.io/getSentiment3.0",
+        {
+          method: "GET",
+          props: {},
+        }
+      );
+
+      const parsedData = JSON.parse(await data.text());
+      console.log(parsedData);
+      setStockInfo();
+    };
+    Promise.all([
+      curStocks.map((name) => {
+        fetchData(name);
+      }),
+    ]);
+  }, [curStocks]);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  const searchStocks = () => {
+    const tempStocks = Object.keys(StockData).filter((stock) => {
+      return stock
+        .toLocaleLowerCase()
+        .includes(searchInput.toLocaleLowerCase());
+    });
+    setCurStocks(tempStocks.slice(0, MAX_STOCKS));
+  };
+
+  const SentimentGraphic = (props) => {
+    return (
+      <div className="sentimentContainer">
+        <div
+          className={
+            "sentimentGraphic" + (props.sentiment === 1 ? " veryBad" : "")
+          }
+        ></div>
+        <div
+          className={"sentimentGraphic" + (props.sentiment === 2 ? " bad" : "")}
+        ></div>
+        <div
+          className={
+            "sentimentGraphic" + (props.sentiment === 3 ? " neutral" : "")
+          }
+        ></div>
+        <div
+          className={
+            "sentimentGraphic" + (props.sentiment === 4 ? " good" : "")
+          }
+        ></div>
+        <div
+          className={
+            "sentimentGraphic" + (props.sentiment === 5 ? " veryGood" : "")
+          }
+        ></div>
+      </div>
+    );
+  };
+
+  const StockContainer = (props) => {
+    console.log(stockInfo);
+    return (
+      <div className="widgetBox">
+        <div>
+          {props.stockName} | {props.stockSymbol}
+        </div>
+        <div>{<SentimentGraphic sentiment={props.stockSentiment} />}</div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <h1>Stocktiment Analysis {StockData["AAPL"]}</h1>
+      <h1>Stocktiment Analysis</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search here"
+          onChange={handleChange}
+          value={searchInput}
+        />
+        <button onClick={searchStocks}>Search</button>
+        {JSON.stringify(stockInfo)}
+      </div>
+      <div className="resultsHolder">
+        {curStocks.map((stock) => (
+          <StockContainer
+            stockName={stock}
+            stockSymbol={StockData[stock]}
+            stockSentiment={stockInfo?.[stock]?.["ovr_rating"]}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- ParseStockData.py
+++ ParseStockData.py
@@ -1,21 +1,14 @@
+import pandas as pd
 import json
 
-f = open("stockData.txt", "r")
-
-data = f.readlines()
-
+data = pd.read_csv('stock.csv')
 parsedData = dict()
 
-for line in data:
-    try:
-        line = line.split('-')
-        line = line[0]
-        symbol, name = line.split('|')
-        symbol = symbol.strip()
-        name = name.strip()
-        parsedData[symbol] = name
-    except:
-        continue
+for i in range(len(data)):
+    curStock = data.iloc[i]
+    value = curStock["Name"].strip("Common Stock").strip("Ordinary Shares").strip("Unit").strip("Units").strip()
+    parsedData[value] = curStock["Symbol"]
 
-with open("sample.json", "w") as outfile:
+
+with open("AllStocks.json", "w") as outfile:
     json.dump(parsedData, outfile)
